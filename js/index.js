641eb695d2e148c6f8f803f4366f2e730a5cfcc8
Nisarg01-01 <82572011+Nisarg01-01@users.noreply.github.com>
2023-06-13 16:12:33 +0530

    error correction

--- js/index.js
+++ js/index.js
@@ -64,5 +64,5 @@ function CreateVcard(){
     link.href = window.URL.createObjectURL(blob);
     link.download = fullname+".vcf";
     link.click();
-    navigator.app.loadUrl("file:///" + contact.name + ".vcf", { openExternal: true });
+    navigator.app.loadUrl("file:///" + fullname + ".vcf", { openExternal: true });
 }
