b53e8e09df9956a2347afb1a05be9e8cebc10bf0
Nisarg01-01 <82572011+Nisarg01-01@users.noreply.github.com>
2023-06-14 17:11:39 +0530

    Changes

--- QRcodeGenerator.py
+++ QRcodeGenerator.py
@@ -6,14 +6,15 @@ designation = "Intern at digitalization"
 company = "Linde Enigneering India Pvt. Ltd."
 phone = "8849904363"
 email = "nisargshah@gmail.com"
-address = "\"Linde House\", Near Nilamber Circle, Vasna Gotri Road, Vasna, Vadodara- 391 410, Gujarat, India"
+address = "Linde House, Near Nilamber Circle, Vasna Gotri Road, Vasna, Vadodara- 391 410, Gujarat, India"
 
 # creare a string to store data in url format
-data = "name="+name+"&designation="+designation+"&phone="+phone+"&email="+email+"&address="+address
+data = "name="+name+"&designation="+designation+"&company="+company+"&phone="+phone+"&email="+email+"&address="+address
 
 # encode it with pybase64
 import pybase64
 data = pybase64.b64encode(data.encode("ascii")).decode("ascii")
 
 qrCode=qrcode.make('https://nisarg01-01.github.io/QR-based-Business-Card/?data='+data, version=1, box_size=10, border=5)
+print("https://nisarg01-01.github.io/QR-based-Business-Card/?data="+data)
 qrCode.save('D:\\Codes\\Projects\\QR-based-Business-Card\\MyLink.png')
