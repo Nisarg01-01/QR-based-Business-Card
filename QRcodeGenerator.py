5527d2a2cd5c27f437e763aa97c0c7a7addddd62
Nisarg01-01 <82572011+Nisarg01-01@users.noreply.github.com>
2023-06-15 10:23:10 +0530

    vcard changes

--- QRcodeGenerator.py
+++ QRcodeGenerator.py
@@ -1,15 +1,17 @@
 import qrcode
 import cv2
 
-name = "Nisarg"
+fname = "Nisarg"
+lname = "Shah"
+fullname = fname + " " + lname
 designation = "Intern at digitalization"
 company = "Linde Enigneering India Pvt. Ltd."
 phone = "8849904363"
 email = "nisargshah@gmail.com"
 address = "Linde House, Near Nilamber Circle, Vasna Gotri Road, Vasna, Vadodara- 391 410, Gujarat, India"
 
-# creare a string to store data in url format
-data = "name="+name+"&designation="+designation+"&company="+company+"&phone="+phone+"&email="+email+"&address="+address
+# creare a string to store data in url format with first name, last name, fullname, designation, company, phone, email, address
+data = "fname="+fname+"&lname="+lname+"&fullname="+fullname+"&designation="+designation+"&company="+company+"&phone="+phone+"&email="+email+"&address="+address
 print(data)
 
 # encode it with pybase64
@@ -19,4 +21,3 @@ data = pybase64.b64encode(data.encode("ascii")).decode("ascii")
 qrCode=qrcode.make('https://nisarg01-01.github.io/QR-based-Business-Card/?data='+data, version=1, box_size=10, border=5)
 qrCode.save('D:\\Codes\\Projects\\QR-based-Business-Card\\MyLink.png')
 # print with local host url
-print('http://127.0.0.1:5500/index.html?data='+data)
--- js/index.js
+++ js/index.js
@@ -22,12 +22,16 @@ function GetCurrentUrl() {
     data = urlParams.get('data');
     data = atob(data);
     // assign the data to the variables 
-    fullname = data.split('&')[0].split('=')[1];
-    designation = data.split('&')[1].split('=')[1];
-    company = data.split('&')[2].split('=')[1];
-    phone = data.split('&')[3].split('=')[1];
-    email = data.split('&')[4].split('=')[1];
-    address = data.split('&')[5].split('=')[1];
+    fname = data.split('&')[0].split('=')[1];
+    lname = data.split('&')[1].split('=')[1];
+    fullname = data.split('&')[2].split('=')[1];
+    designation = data.split('&')[3].split('=')[1];
+    company = data.split('&')[4].split('=')[1];
+    phone = data.split('&')[5].split('=')[1];
+    email = data.split('&')[6].split('=')[1];
+    address = data.split('&')[7].split('=')[1];
+    console.log(fname);
+    console.log(lname);
     console.log(fullname);
     console.log(designation);
     console.log(company);
@@ -68,7 +72,7 @@ function GoToLocation(){
 function CreateVcard(){
     // var vcard = "BEGIN:VCARD\nVERSION:3.0\nFN:"+fullname+"\nORG: "+designation+"\nTEL;TYPE=WORK,VOICE:"+phone+"\nADR;TYPE=WORK:;;"+address+"\nEMAIL:"+email+"\nEND:VCARD";
     // create a vcard with org as company and nickname as designation both are compulsory fields
-    var vcard = "BEGIN:VCARD\nVERSION:3.0\nFN:"+fullname+"\nNICKNAME:"+designation+"\nORG: "+company+"\nTEL;TYPE=WORK,VOICE:"+phone+"\nADR;TYPE=WORK:;;"+address+"\nEMAIL:"+email+"\nEND:VCARD";        
+    var vcard = "BEGIN:VCARD\nVERSION:3.0\nN:"+lname+";"+fname+"\nFN:"+fullname+"\nORG:"+company+"\nTITLE:"+designation+"\nTEL;TYPE=WORK,VOICE:"+phone+"\nADR;TYPE=WORK:;;"+address+"\nEMAIL:"+email+"\nEND:VCARD";
     var blob = new Blob([vcard], {type: "text/vcard"});
     var link = document.createElement('a'); 
     link.href = window.URL.createObjectURL(blob);
